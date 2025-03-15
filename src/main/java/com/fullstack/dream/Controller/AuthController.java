c8cf4662b8d22e82d6e18f082d72fa37f7868b65
ckumar13 <chinmay_kumar@intuit.com>
2025-03-16 03:34:42 +0530

    First Backend Commit

---
+++ src/main/java/com/fullstack/dream/Controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.fullstack.dream.Controller;
+
+import com.fullstack.dream.Service.UserService;
+import com.fullstack.dream.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("register")
+    public ResponseEntity<?> registerUser(@RequestBody User user){
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("signin")
+    public ResponseEntity<?> loginUser(@RequestBody User loginUser){
+        return userService.login(loginUser);
+    }
+}
